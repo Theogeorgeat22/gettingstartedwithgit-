dd05ce8c4077d304b50cc10200b061bb3f1a7511
Theo  George <tgeorge@my365-uwc.ac.za>
2020-10-22 19:21:32 +0000

    my first black of code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace gettingstartedwithgit_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Theo";
+            string Lastname="Black";
+            int Age=27;
+
+            if(Age==27 ){
+                Console.WriteLine($"My name is {name } and surname {Lastname } and age {Age}"); 
+
+
+            } else{
+                Console.WriteLine($"input is wrong  ");
+
+            }  
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
